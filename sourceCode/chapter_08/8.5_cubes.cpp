4377542d5483e8df337aab0a20213e4da82a65e2
xm <473047907@qq.com>
2025-09-23 16:42:26 +0800

    regular and reference arguments

---
+++ sourceCode/chapter_08/8.5_cubes.cpp
@@ -0,0 +1,29 @@
+// cubes.cpp -- regular and reference arguments
+#include <iostream>
+
+double cube(double a);
+double refcube(double& ra);
+
+int main()
+{
+    double x = 3.0; 
+
+    std::cout << cube(x);
+    std::cout << " = cube of " << x << std::endl;
+    std::cout << refcube(x);
+    std::cout << " = cube of " << x << std::endl;
+
+    return 0;
+}
+
+double cube(double a)
+{
+    a *= a * a;
+    return a;
+}
+
+double refcube(double& ra)
+{
+    ra *= ra * ra;
+    return ra;
+}
