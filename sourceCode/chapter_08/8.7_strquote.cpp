ca1f770cc9b29b8db4a987bf9343074ca7c407b0
xm <473047907@qq.com>
2025-09-24 09:52:49 +0800

    difference designs

---
+++ sourceCode/chapter_08/8.7_strquote.cpp
@@ -0,0 +1,56 @@
+// strquote.cpp -- difference designs
+#include <iostream>
+#include <string>
+
+std::string version1(const std::string& s1, const std::string& s2);
+const std::string& version2(std::string& s1, const std::string& s2);
+const std::string& version3(std::string& s1, const std::string& s2);
+
+int main()
+{
+    std::string input;
+    std::string copy;
+    std::string result;
+
+    std::cout << "enter a string: " << std::endl;
+    std::getline(std::cin, input);
+    copy = input; 
+    std::cout << "your string as entered: " << input << std::endl;
+
+    result = version1(input, "***");
+    std::cout << "your string enhanced: " << result << std::endl;
+    std::cout << "your original string: " << input << std::endl;
+
+    result = version2(input, "###");
+    std::cout << "your string enhanced: " << result << std::endl;
+    std::cout << "your original string: " << input << std::endl;
+
+    std::cout << "resetting original string" << std::endl;
+    input = copy;
+    result = version3(input, "@@@");
+    std::cout << "your string enhanced: " << result << std::endl;
+    std::cout << "your original string: " << input << std::endl;
+    return 0;
+}
+
+std::string version1(const std::string& s1, const std::string& s2)
+{
+    std::string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+}
+
+const std::string& version2(std::string& s1, const std::string& s2)
+{
+    s1 = s2 + s1 + s2;
+    // safe to return reference passed to function
+    return s1;
+}
+
+const std::string& version3(std::string& s1, const std::string& s2)
+{
+    std::string temp;
+    temp = s2 + s1 + s2;
+    // unsafe to return reference to local variable
+    return temp;
+}
