7c4fda573a83df2a4134e5d840e62596282706ec
xm <473047907@qq.com>
2025-11-12 09:23:56 +0800

    use operator <<

---
+++ sourceCode/chapter_11/11.12_usetime3.cpp
@@ -0,0 +1,23 @@
+// usetime3.cpp -- using the fourth draft of the Time class
+//  compile usetime3.cpp and mytime3.cpp together
+#include <iostream>
+
+#include "11.10_mytime3.h"
+
+int main()
+{
+    Time aida(3, 35);
+    Time tosca(2, 48);
+    Time temp;
+
+    std::cout << "Aida and tosca:\n";
+    std::cout << aida << "; " << tosca << std::endl;
+    temp = aida + tosca;
+    std::cout << "Aida + Tosca: " << temp << std::endl;
+
+    temp = aida * 1.17;
+    std::cout << "Aida * 1.17 :" << temp << std::endl;
+    std::cout << "10.0 * Tosca: " << 10.0 * tosca << std::endl;
+
+    return 0;
+}
