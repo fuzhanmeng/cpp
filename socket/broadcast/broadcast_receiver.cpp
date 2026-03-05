0d2ec9ce2926c2084b26013f51d28f58620bff1d
xm <473047907@qq.com>
2026-03-05 16:56:26 +0800

    udp broadcast

---
+++ socket/broadcast/broadcast_receiver.cpp
@@ -0,0 +1,74 @@
+#include <arpa/inet.h>
+#include <asm-generic/socket.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <array>
+#include <iostream>
+
+constexpr int kServerPort = 8888;
+constexpr int kBufferSize = 1024;
+
+int main() {
+    // 1. 创建 UDP socket
+    int server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_fd == -1) {
+        std::cout << "创建socket失败" << std::endl;
+        return -1;
+    }
+
+    // 端口重用
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
+        std::cout << "端口重用失败" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    // 2. 填充服务器地址
+    struct sockaddr_in server_addr {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(kServerPort);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // 3. 绑定信箱
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        std::cout << "绑定失败" << std::endl;
+        close(server_fd);
+        return -1;
+    }
+
+    std::cout << "UDP 服务器启动成功... " << std::endl;
+
+    std::array<char, kBufferSize> buffer{};
+
+    // 4. 开启极简循环：收信 -> 回信
+    while (true) {
+        struct sockaddr_in client_addr {};
+        socklen_t client_len = sizeof(client_addr);
+        buffer.fill('\0');
+
+        ssize_t bytes_recv =
+            recvfrom(server_fd, buffer.data(), buffer.size() - 1, 0, (struct sockaddr*)&client_addr, &client_len);
+
+        if (bytes_recv > 0) {
+            std::cout << "收到客户端消息: " << buffer.data() << std::endl;
+
+            // 💡 第一个参数必须是 server_fd！
+            ssize_t bytes_send =
+                sendto(server_fd, buffer.data(), bytes_recv, 0, (struct sockaddr*)&client_addr, client_len);
+            if (bytes_send > 0) {
+                std::cout << "回信成功！" << std::endl;
+            } else {
+                std::cout << "回信失败！" << std::endl;
+            }
+        } else if (bytes_recv < 0) {
+            std::cout << "网络接收发生异常" << std::endl;
+            break;
+        }
+    }
+
+    close(server_fd);
+    return 0;
+}
---
+++ socket/broadcast/broadcast_send.cpp
@@ -0,0 +1,85 @@
+#include <arpa/inet.h>
+#include <asm-generic/socket.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <array>
+#include <iostream>
+#include <string>
+
+constexpr int kServerPort = 8888;
+constexpr int kBufferSize = 1024;
+
+int main() {
+    // 1. 创建socket fd
+    int client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_fd == -1) {
+        std::cout << "创建client fd 失败" << std::endl;
+        return -1;
+    }
+
+    // 2. 向操作系统申请广播特权
+    int opt = 1;
+    if (setsockopt(client_fd, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt)) == -1) {
+        std::cout << "申请广播特权失败" << std::endl;
+        close(client_fd);
+        return -1;
+    }
+
+    // 3. 要发给哪个服务器
+    struct sockaddr_in server_addr {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(kServerPort);
+    if (inet_pton(AF_INET, "255.255.255.255", &server_addr.sin_addr.s_addr) != 1) {
+        std::cout << "地址转换失败" << std::endl;
+    }
+
+    // 4. 直接开始收发消息
+    std::string input_msg;
+    std::array<char, kBufferSize> buffer{};
+
+    std::array<char, INET_ADDRSTRLEN> server_ip;
+    if (inet_ntop(AF_INET, &server_addr.sin_addr.s_addr, server_ip.data(), server_ip.size()) != nullptr) {
+        std::cout << "网络IP转字符IP成功" << std::endl;
+    }
+
+    while (true) {
+        std::cout << "send message: " << std::endl;
+        std::getline(std::cin, input_msg);
+        if (input_msg.empty()) {
+            std::cout << "输入了空格，继续" << std::endl;
+            continue;
+        }
+
+        if (input_msg == "quit") {
+            std::cout << "输入了 quit,退出" << std::endl;
+            close(client_fd);
+            break;
+            ;
+        }
+
+        ssize_t bytes_send = sendto(client_fd, input_msg.c_str(), input_msg.size(), 0, (struct sockaddr*)&server_addr,
+                                    sizeof(server_addr));
+        if (bytes_send > 0) {
+            std::cout << "send messge success" << std::endl;
+        } else if (bytes_send == -1) {
+            std::cout << "send message fail" << std::endl;
+        }
+
+        // 开始接收
+        buffer.fill('\0');
+        ssize_t bytes_recv = recvfrom(client_fd, buffer.data(), buffer.size() - 1, 0, nullptr, nullptr);
+        if (bytes_recv > 0) {
+            std::cout << "recv message from " << server_ip.data() << ", message: " << buffer.data() << std::endl;
+        } else if (bytes_recv == 0) {
+            std::cout << "服务器断开了连接" << std::endl;
+        } else {
+            std::cout << "崩溃中断" << std::endl;
+        }
+    }
+
+    close(client_fd);
+    return 0;
+}
